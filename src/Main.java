075a60b5e1c84d2ce2b30fb621bc1af32932b7e1
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-01 15:11:28 +0300

    instanciate car class

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         //example: (phone, desk,computer,coffee cup)
         Car myCar1 = new Car();
         Car myCar2 = new Car();
-//        System.out.println(myCar.model);
-//        System.out.println(myCar.year);
-//        System.out.println(myCar.make);
-//        System.out.println(myCar.color);
-//        System.out.println(myCar.price);
+        System.out.println(myCar.model);
+        System.out.println(myCar.year);
+        System.out.println(myCar.make);
+        System.out.println(myCar.color);
+        System.out.println(myCar.price);
 
         myCar1.drive();
         myCar1.brake();
