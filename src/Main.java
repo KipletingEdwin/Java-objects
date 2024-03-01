3872cc4134fca65030fa6cbcda0f67bf4780215a
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-01 15:10:44 +0300

    instanciate two car class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-(//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //object- an instance of a class that may contain attributes and methods
         //example: (phone, desk,computer,coffee cup)
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+//        System.out.println(myCar.model);
+//        System.out.println(myCar.year);
+//        System.out.println(myCar.make);
+//        System.out.println(myCar.color);
+//        System.out.println(myCar.price);
+
+        myCar1.drive();
+        myCar1.brake();
+
+        myCar2.drive();
+        myCar2.brake();
 
         }
     }
-}
